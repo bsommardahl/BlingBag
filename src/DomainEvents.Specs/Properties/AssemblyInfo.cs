18a60ffd58612840f2edb1227062fcc1353363ac
Byron Sommardahl <byron@acklenavenue.com>
2012-05-25 21:06:02 -0500

    Removed more acklen avenue stuff.

--- src/DomainEvents.Specs/Properties/AssemblyInfo.cs
+++ src/DomainEvents.Specs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AcklenAvenue.DomainEvents.Specs")]
+[assembly: AssemblyTitle("DomainEvents.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AcklenAvenue.DomainEvents.Specs")]
+[assembly: AssemblyProduct("DomainEvents.Specs")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
